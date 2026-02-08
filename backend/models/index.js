dac8de99c456b14ce1da3b66b19758995fd166fd
simonkaruga <simonkaruganjoroge@gmail.com>
2026-02-08 23:58:55 +0300

    Fix: Use config.js to support DATABASE_URL in production

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,10 +2,15 @@
 
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-let sequelize = new Sequelize(config.database, config.username, config.password, config);
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 // User model uses raw SQL
 db.User = require('./user');
